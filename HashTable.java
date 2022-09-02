e3a0e347d4c21f95baa6a7d805db54ea25d60ae9
radheashu <ashwinihanwate1020@gmail.com>
2022-09-02 22:47:38 +0530

    added:New Programs

---
+++ HashTable.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class HashTable {
+        public static void main(String[] args) {
+            HashTable<String, Integer> HashTable = new HashTable ();
+            String message = "To be or not to be";
+
+            //Split the words when a white space comes up.
+            //Convert all the words in lowercase
+            String[] messageArray = message.toLowerCase().split(" ");
+
+            for (String word : messageArray) {
+                //Search for the word in the linked list
+                Integer value = hashImpl.get(word);
+                //If not found, frequency of the word StayWith 1
+                if (value == null)
+                    value = 1;
+                else
+                    //If found, frequency of the word increases by 1
+                    value = value + 1;
+                hashImpl.add(word, value);
+            }
+            System.out.println(hashImpl);
+        }
+    }
+
